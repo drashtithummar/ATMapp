0c468347337f1e9bc478eac114fd974399a5c77a
Drashti Thummar <59496853+drashtithummar@users.noreply.github.com>
2023-07-12 14:33:24 -0400

    Third commit
    Added more features

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace ATMApp
+namespace ATM
 {
     public class Program
     {
@@ -15,59 +10,107 @@ namespace ATMApp
             int pin = 1514;
 
             int tries = 3;
+            bool isCardBlocked = false;
 
-            while (tries != 0)
+            while (tries != 0 && !isCardBlocked)
             {
                 Console.WriteLine("Enter Your PIN: ");
                 int user_pin = Convert.ToInt32(Console.ReadLine());
 
                 if (user_pin == pin)
                 {
-                    Console.WriteLine("Enter the transaction type you wish to perform: \n D: Deposit \n W: withdrawal");
-                    string transType = Console.ReadLine();
+                    Console.WriteLine("Login successful!");
 
-                    if (transType.Equals("D") || transType.Equals("d"))
+                    while (true)
                     {
-                        Console.WriteLine("Enter the amount you want to desposit: ");
-                        double amnt = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("\nSelect an option: ");
+                        Console.WriteLine("1. Check Balance");
+                        Console.WriteLine("2. Deposit");
+                        Console.WriteLine("3. Withdraw");
+                        Console.WriteLine("4. Transfer Funds");
+                        Console.WriteLine("5. Exit");
 
-                        balance = Deposit(balance, amnt);
+                        int option = Convert.ToInt32(Console.ReadLine());
 
-                        Console.WriteLine("\n Your transaction was successfull.\n Your balance is : " + balance);
-                    }
-                    else if (transType.Equals("W") || transType.Equals("w"))
-                    {
-                        Console.WriteLine("Enter the amount you want to withdraw: ");
-                        double amnt = Convert.ToDouble(Console.ReadLine());
-                        balance = Withdraw(balance, amnt);
-                        Console.WriteLine("\n Your transaction was successfull.\n Your balance is : " + balance);
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("Your current balance is: " + balance);
+                                break;
 
+                            case 2:
+                                Console.WriteLine("Enter the amount you want to deposit: ");
+                                double depositAmount = Convert.ToDouble(Console.ReadLine());
+                                balance = Deposit(balance, depositAmount);
+                                Console.WriteLine("Your deposit was successful. Your new balance is: " + balance);
+                                break;
+
+                            case 3:
+                                Console.WriteLine("Enter the amount you want to withdraw: ");
+                                double withdrawAmount = Convert.ToDouble(Console.ReadLine());
+
+                                if (withdrawAmount <= balance)
+                                {
+                                    balance = Withdraw(balance, withdrawAmount);
+                                    Console.WriteLine("Your withdrawal was successful. Your new balance is: " + balance);
+
+                                    Console.WriteLine("Enter the denomination of notes you want to withdraw (e.g., 20, 50): ");
+                                    int noteDenomination = Convert.ToInt32(Console.ReadLine());
+                                    int numberOfNotes = (int)(withdrawAmount / noteDenomination);
+
+                                    Console.WriteLine("Dispensing $" + withdrawAmount + " as " + noteDenomination + " notes: " + numberOfNotes);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Insufficient funds!");
+                                }
+                                break;
+
+                            case 4:
+                                Console.WriteLine("Enter the amount you want to transfer: ");
+                                double transferAmount = Convert.ToDouble(Console.ReadLine());
+
+                                if (transferAmount <= balance)
+                                {
+                                    Console.WriteLine("Enter the recipient's account number: ");
+                                    string recipientAccount = Console.ReadLine();
+
+                                    // Perform the transfer logic here
+
+                                    balance = Withdraw(balance, transferAmount);
+                                    Console.WriteLine("Your transfer was successful. Your new balance is: " + balance);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Insufficient funds!");
+                                }
+                                break;
+
+                            case 5:
+                                Console.WriteLine("Thank you for using the ATM. Goodbye!");
+                                Environment.Exit(0);
+                                break;
+
+                            default:
+                                Console.WriteLine("Invalid option! Please select a valid option.");
+                                break;
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine("Invalid transaction type");
-                    }
-                    break;
                 }
-
                 else
                 {
                     tries--;
                     if (tries > 0)
                     {
-                        Console.WriteLine("Inconrrect PIN \n\n Try again");
+                        Console.WriteLine("Incorrect PIN. " + tries + " attempts remaining. Please try again.");
                     }
                     else if (tries == 0)
                     {
-                        Console.WriteLine("Incorrect PIN \n Card Blocked!!");
+                        Console.WriteLine("Incorrect PIN. Card blocked!!");
+                        isCardBlocked = true;
                     }
-
                 }
-
-
-
             }
-
 
             Console.ReadKey();
         }
@@ -83,5 +126,3 @@ namespace ATMApp
         }
     }
 }
-
-
